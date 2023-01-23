74f5dbd3ac11b393695642ee8a344f2d67e615fd
yudainemot0 <majesticsurfboard@gmail.com>
2023-01-24 07:29:34 +0900

    ✨ カテゴリーページ実装

---
+++ pages/category/[id].js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { client } from "@/libs/client";
+
+export default function CategoryId({ blog }) {
+  // カテゴリーに紐付いたコンテンツがない場合
+  if (blog.length === 0) {
+    return <div>ブログコンテンツがありません</div>;
+  }
+  return (
+    <div>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// 静的生成のパス
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "categories" });
+  const paths = data.contents.map((content) => `/category/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({
+    endpoint: "blog",
+    queries: { filters: `category[equals]${id}` },
+  });
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Link from "next/link";
 import { client } from "../libs/client";
 
-export default function Home({ blog }) {
+export default function Home({ blog, category }) {
   return (
     <div>
+      <ul>
+        {category.map((category) => (
+          <li key={category.id}>
+            <Link href={`/category/${category.id}`}>{category.name}</Link>
+          </li>
+        ))}
+      </ul>
       <ul>
         {blog.map((blog) => (
           <li key={blog.id}>
@@ -15,12 +22,15 @@ export default function Home({ blog }) {
   );
 }
 
+// データをテンプレートに受け渡す部分の処理
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blog" });
+  const categoryData = await client.get({ endpoint: "categories" });
 
   return {
     props: {
       blog: data.contents,
+      category: categoryData.contents,
     },
   };
 };
