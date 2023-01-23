333c438d842c0fed0560d59a9422dd6462c846b8
yudainemot0 <majesticsurfboard@gmail.com>
2023-01-23 21:52:39 +0900

    ✨ カテゴリー名追加

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,11 +1,12 @@
 import { client } from "../../libs/client";
-import styles from "../../styles/Home.module.scss"
+import styles from "../../styles/Home.module.scss";
 
 export default function BlogId({ blog }) {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{blog.title}</h1>
       <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <p>{blog.category && blog.category.name}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
