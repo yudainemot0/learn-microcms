9d231c64a8d54afc53c870f68e5c712cc9429431
yudainemot0 <majesticsurfboard@gmail.com>
2023-01-26 07:11:55 +0900

    ✨ ページネーション実装

---
+++ components/Pagination.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export const Pagination = ({ totalCount }) => {
+  const PER_PAGE = 5;
+
+  const range = (start, end) =>
+    [...Array(end - start + 1)].map((_, i) => start + i);
+  return (
+    <ul>
+      {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
+        <li key={index}>
+          <Link href={`/blog/page/${number}`}>{number}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import { client } from "../libs/client";
+import { Pagination } from "@/components/Pagination";
 
-export default function Home({ blog, category }) {
+export default function Home({ blog, totalCount }) {
   return (
     <div>
-      <ul>
+      {/* <ul>
         {category.map((category) => (
           <li key={category.id}>
             <Link href={`/category/${category.id}`}>{category.name}</Link>
           </li>
         ))}
-      </ul>
+      </ul> */}
       <ul>
         {blog.map((blog) => (
           <li key={blog.id}>
@@ -18,19 +19,24 @@ export default function Home({ blog, category }) {
           </li>
         ))}
       </ul>
+      <Pagination totalCount={totalCount} />
     </div>
   );
 }
 
 // データをテンプレートに受け渡す部分の処理
 export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blog" });
-  const categoryData = await client.get({ endpoint: "categories" });
+  const data = await client.get({
+    endpoint: "blog",
+    queries: { offset: 0, limit: 5 },
+  });
+  // const categoryData = await client.get({ endpoint: "categories" });
 
   return {
     props: {
       blog: data.contents,
-      category: categoryData.contents,
+      // category: categoryData.contents,
+      totalCount: data.totalCount,
     },
   };
 };
